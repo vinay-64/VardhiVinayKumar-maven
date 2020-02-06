44f3b335048fe3ef24cc7d99547beaee204a77f6
vinaykumar1237 <56100155+vinaykumar1237@users.noreply.github.com>
2020-02-06 23:08:37 +0530

    Update Halwa.java

--- src/main/java/newyeargift/Halwa.java
+++ src/main/java/newyeargift/Halwa.java
@@ -1,5 +1,12 @@
 package newyeargift;
 
-public class Halwa {
+public class Halwa extends Sweets{
+  public Halwa() {
+    }
+
+    public Halwa(double aSugar, double aWeight) {
+        setSugarLevel(aSugar);
+        setWeight(aWeight);
+    }
 
 }
