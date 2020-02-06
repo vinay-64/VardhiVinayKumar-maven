e1a6b86b545d27771210a39dbf0112da014f078a
vinaykumar1237 <56100155+vinaykumar1237@users.noreply.github.com>
2020-02-06 23:04:12 +0530

    Update Darkchocolate.java

--- src/main/java/newyeargift/Darkchocolate.java
+++ src/main/java/newyeargift/Darkchocolate.java
@@ -1,5 +1,13 @@
 package newyeargift;
 
-public class Darkchocolate {
+public class Darkchocolate extends Sweets {
+  public DarkChocolate() {
+    }
+
+    public DarkChocolate(double aSugarLevel, double aWeight) {
+        setSugarLevel(aSugarLevel);
+        setWeight(aWeight);
+    }
+
 
 }
