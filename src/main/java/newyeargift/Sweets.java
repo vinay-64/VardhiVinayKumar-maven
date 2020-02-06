d76206397d55dd8e837ae8da25a67cb63365c693
vinaykumar1237 <56100155+vinaykumar1237@users.noreply.github.com>
2020-02-06 23:02:46 +0530

    Update Sweets.java

--- src/main/java/newyeargift/Sweets.java
+++ src/main/java/newyeargift/Sweets.java
@@ -1,5 +1,31 @@
 package newyeargift;
 
 public class Sweets {
+  private double sugarLevel;
+
+    private double weight;
+
+    public double getSugarLevel() {
+        return sugarLevel;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setSugarLevel(double sugarLevel) {
+        this.sugarLevel = sugarLevel;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName() + " " + sugarLevel + " " + weight);
+        return sb.toString();
+    }
+
 
 }
